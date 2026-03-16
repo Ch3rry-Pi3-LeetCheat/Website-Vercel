ebec6e227b37fbe61f397a8d317b397ade043f2e
Roger Campbell <the_rfc@hotmail.co.uk>
2026-03-16 15:11:56 +0000

    maths lin alg 1

--- components/math/AlgebraStaticVisual.tsx
+++ components/math/AlgebraStaticVisual.tsx
@@ -653,7 +653,7 @@ export default function AlgebraStaticVisual({
               <line x1="222" y1="101" x2="316" y2="145" stroke="rgba(231,238,248,1)" strokeWidth="1.8" markerEnd="url(#pythag-side-arrow)" />
 
               {/* Side labels are positioned with foreignObject x/y only; width/height rarely need touching. */}
-              <foreignObject x="62" y="90" width="24" height="28">
+              <foreignObject x="65" y="85" width="24" height="28">
                 <div className="flex h-full items-center justify-center text-center">
                   <MathInline
                     tex={String.raw`{\color{#f472b6}a}`}
